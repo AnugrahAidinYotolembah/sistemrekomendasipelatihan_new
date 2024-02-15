c6d21955275c12dab6565f7e02862c815a0293e5
AnugrahAidinYotolembah <anugrahaidinyotolembah19@gmail.com>
2024-02-15 21:08:28 +0800

    project : sistem rekomendasi pelatihan terbaik

---
+++ app.py
@@ -0,0 +1,278 @@
+import streamlit as st
+import pandas as pd
+import joblib
+from streamlit_folium import folium_static
+import folium
+
+
+
+
+
+def get_recommendations(need, cosine_sim, data):
+    search_recommendations = len(data)
+    idx = data.loc[(data['need'] == need)].index[0]
+    sim_scores = list(enumerate(cosine_sim[idx]))
+    sim_scores = sorted(sim_scores, key=lambda x: x[1], reverse=True)
+    sim_scores = sim_scores[1:search_recommendations+1]
+    company_indices = [i[0] for i in sim_scores]
+    return data[['training_name']].iloc[company_indices].drop_duplicates()
+
+def need_recommendation(need, data, cosine_sim):
+    recommendations = get_recommendations(need, cosine_sim, data)[:10]
+    recommendations['score'] = range(1, len(recommendations) + 1)
+    recommendations_sorted = recommendations.sort_values(by='score', ascending=True)
+
+    for i, row in recommendations_sorted.iterrows():
+        score_percent = (len(recommendations) - row['score'] + 1) / len(recommendations) * 100
+        result_string = f"{row['training_name']} (compatibility: {score_percent:.2f}%)"
+        recommendations_sorted.loc[i, 'training_name'] = result_string
+
+    recommendations_sorted = recommendations_sorted.drop(['score'], axis=1)
+    dict_from_df = recommendations_sorted.to_dict(orient='list')
+    return dict_from_df
+
+
+
+
+
+
+
+
+
+# Fungsi utama
+def main():
+    st.set_page_config(page_title="Rekomendasi Produk Pelatihan", page_icon="image/artificial-intelligence (1).png", layout="wide", initial_sidebar_state="collapsed", menu_items=None)
+    # Mengatur warna latar belakang menjadi putih
+    page_bg_img = '''
+    <style>
+    body {
+    background-image: url("https://images.unsplash.com/photo-1542281286-9e0a16bb7366");
+    background-size: cover;
+    }
+    footer {visibility: hidden;}
+    @media only screen and (max-width: 600px) {
+    /* Atur gaya tata letak untuk layar seluler */
+    body {
+    background-image: none;  /* Hapus gambar latar belakang untuk layar seluler */
+    background-color: #ffffff;  /* Ganti latar belakang dengan warna putih */ 
+    }
+    }
+    </style>
+    '''
+    st.markdown(page_bg_img, unsafe_allow_html=True)
+
+
+    # Menambahkan sidebar dengan foto, nama, dan nim
+    def home_page():
+        st.title("Home")
+        st.title("Selamat Datang Kembali")
+            
+        
+    def profile_page():
+        st.title("Profile")
+        st.image("image/Anugrah Aidin Yotolembah_F55120093.jpg", use_column_width=True)
+        st.title("Anugrah Aidin Yotolembah")
+        st.title("F55120093")
+        st.title("S1 Teknik Informatika")
+        st.title("Universitas Tadulako")
+
+    with st.sidebar:
+        selected = st.selectbox(" ", ["Home", 'Profile'], index=0) 
+        
+
+        if selected == "Home":
+            home_page()
+        elif selected == "Profile":
+            profile_page()
+
+    # Judul halaman dengan warna dan style
+    st.markdown(
+        """
+        <h2 style='text-align: center; color: #FFFFFF;'>Implementasi Sistem Rekomendasi Penjualan produk Pelatihan Terbaik menggunakan algoritma Cosine Similarity<br>(studi kasus : PT MENARA INDONESIA)</h2>
+        <hr style='border: 2px solid #182c25;'>
+        <br>
+        <h2 style='text-align: center; color: #FFFFFF;'>Katalog Penjualan Pelatihan</h2>
+        <br>
+        """,
+        unsafe_allow_html=True
+    )
+
+    image_path1 = "image/gambar_1.png"  # Ganti dengan path gambar yang sesuai
+    st.image(image_path1, use_column_width=True)
+    image_path2 = "image/gambar_2.png"  # Ganti dengan path gambar yang sesuai
+    st.image(image_path2, use_column_width=True)
+    image_path3 = "image/gambar_3.png"  # Ganti dengan path gambar yang sesuai
+    st.image(image_path3, use_column_width=True)
+    image_path4 = "image/gambar_4.png"  # Ganti dengan path gambar yang sesuai
+    st.image(image_path4, use_column_width=True)
+   
+    # Visualisasi peta Indonesia dengan folium
+    st.markdown("---")
+    st.markdown("<h2 style='text-align: center; color: #FFFFFF;'>daerah pelatihan yang telah dikunjungi</h2>", unsafe_allow_html=True)
+        
+    # Contoh peta statis menggunakan folium
+    m = folium.Map(location=[-2.5489, 118.0149], zoom_start=5)  # Koordinat tengah Indonesia
+
+    # Menambahkan marker pada peta (contoh)
+    folium.Marker([-6.2088, 106.8456], popup='Jakarta').add_to(m)
+    folium.Marker([-7.2504, 112.7688], popup='Surabaya').add_to(m)
+    folium.Marker([-6.595038, 106.816635], popup='Bogor').add_to(m)
+    folium.Marker([-6.966667, 110.416664], popup='Semarang').add_to(m)
+    folium.Marker([-6.347891, 106.741158], popup='Tangerang Selatan').add_to(m)
+    folium.Marker([1.474830, 124.842079], popup='Manado').add_to(m)
+    folium.Marker([-6.905977, 107.613144], popup='Bandung').add_to(m)
+    folium.Marker([-1.609972, 103.607254], popup='Jambi').add_to(m)
+    folium.Marker([-7.797068, 110.370529], popup='Yogyakarta').add_to(m)
+    folium.Marker([-6.966667, 110.416664], popup='Semarang').add_to(m)
+    folium.Marker([-6.178306, 106.631889], popup='Tangerang').add_to(m)
+    
+
+    # Menampilkan peta
+    # Menampilkan peta dengan responsif
+    folium_static(m, width=940, height=500)
+
+    make_map_responsive= """
+        <style>
+        [title~="st.iframe"] { width: 100%}
+        </style>
+        """
+    st.markdown(make_map_responsive, unsafe_allow_html=True)
+    
+    st.markdown(
+        """
+        <hr style='border: 2px solid #182c25;'>
+        <br>
+        <h2 style='text-align: center; color: #FFFFFF;'>Rekomendasi Produk Pelatihan Terbaik</h2>
+        """,
+        unsafe_allow_html=True
+        )
+
+
+    # Pilihan list up-down untuk kebutuhan pelatihan
+    options = ["Production Control With Kanban","ProResolve Production Genius","TroubleSolver TechCraft",
+               "InnovateEase Production Solutions", "MasterMind ProblemSolve Suite","Project Management & Assertive User Handling",
+               "Modern Project Management: Telling, Doodly, Storyline and Video-Sound",
+               "Compherensive Train The Trainers (Indoor-outdoor) Activity",
+               "Train The Trainer For Outbound",
+               "Mastering 10 Core Trainer’s Tools", "Design Thinking: Creating Innovative Product, Excellent Services and Powerful Selling",
+               "Innovation Catalyst",
+               "Pusat Inovasi Pengembangan Produk",
+               "Innovation Solutions Suite for Product Development",
+               "Mastery Tools for Product Innovation", "Creative Promotion & Campaign Management",
+               "Innovative Business Thinking & Productive",
+               "Innovative Leadership for Changes",
+               "Combining Scrum & Sprint",
+               "Creativity with IDEO & XQ Principles","Pemahaman Pasar TrenAnalitik",
+               "Marketing Research For Business Using SPSS",
+               "TrendProbe Advanced Research","Trend Market Analysis","Navigator Tren Pasar Data riset", "Leaders As A Coach & Sales Team Developer",
+               "Leaders as a Coach & Team Developer in Collection",
+               "Paket Pengawasan MonitorWise", "Guardian Tools for Smart Monitoring", "Watchtower Tech for SupervisionPlus","Risk & Governance in Digital Era",
+               "Information Security Awareness",
+               "Electronic Filing & Document Management System",
+               "Understanding Millenials - Managing for High Performance and Engagement", "Professional Digital Marketing",
+               "Extraordinary Digital Presentation, Public Speaking & Training Delivery Skills", "Pandai Bicara Profesional Suite",
+               "KomunikaCerdas Pelatihan Bahasa", "Suaraku Jelas Modul Komunikasi", "Optimalisasi Social Media Branding Menggunakan Storytelling",
+               "Presentation & Selling Skills Using Story-Telling", "ExpressVista Communication Canvas",
+               "ImagineFlow Creative Communique", "Dinamika Kreatif TrenTalk", "Warehouse Administration for Better Complete Beginner",
+               "Manajemen Pembelian, PPIC, Pergudangan & Negosiasi Pengadaan","perencanaan produksi Cerdas",
+               "Strategi finansial dalam  Perencana produksi",
+               "perencanaan Optimalisasi Pengelolaan Produksi", "Hak dan Kewajiban Pegawai di Era New Normal: Adaptasi Kebijakan Baru pasca Covid 19",
+               "Hukum Bisnis Indonesia Consult", "Legal Maju Pengetahuan Bisnis IndoLaw",
+               "Aspek Hukum CorporateWise IndoLegal", "Hukum Perusahaan Indonesia", "System Application & Technology Knowledge & Skill for Transaction",
+               "Electronic Filing preparing for BlockChain, IOT",
+               "bantuan layanan Troubleshoot di indonesia", "Mitra Teknologi Dukungan IT Cerdas",
+               "Solusi Bantuan Teknologi troubleshoot", "Legal Drafting to Prevent and Anticipate",
+               "Risk Analysis: to Reduce Losses and Increase Profits", "Aspek Legal Pembiayaan & Penagihan Kartu Kredit",
+               "LawGuard Compliance Solutions", "Wawasan Hukum ComplianceCraft", "Akuisisi Produk Financial Services Melalui Digital Marketing",
+               "Program Penguasaan SkillMinds","InnovateLearn Workshop Series", "CreativeFlow Training Experience",
+               "Akselerator TalentCraft", "Legal Drafting to Prevent and Anticipate",
+               "Strategic Partnership - How to Optimize Partnership Efforts for Your Strategic Goals in New Normal Era",
+               "Kursus Penguasaan Wawasan Hukum", "Program Keterampilan Analisis LawCraft",
+               "Pelatihan program Analisis Hukum LegalMind", "Excel & PowerPoint for Powerful Business Application",
+               "Pelatihan Profesional OfficeSkills", "EfficientOffice Mastery Program", "AdminTech Essentials Workshop Series",
+               "DataMastery Administrative Training", "How To Conduct TNA, Creating Training Material & Measuring Training Effectivity",
+               "ScholarCraft Research Intensive", "Pelatihan Keunggulan Penelitian DiscoverMind", "ScholarCraft Research Intensive",
+               "DiscoverMind Research Excellence Training", "Powerful Database Analysis & Dashbord Reporting with Excel",
+               "Applied Debt Collection Strategy & Tactics", "DataHarbor Analytics Mastery",
+               "Insightful Data Mining Techniques", "Strategic Data Collection Tactics", "Leaders As A Coach & Sales Team Developer","Leaders as a Coach & Team Developer in Collection",
+               "Penguasaan Komunikasi untuk Pimpinan Tim", "Membangun Tim Efektif melalui Komunikasi",
+               "Leadership Communication Strategies", "Project Management & Assertive User Handling",
+               "Modern Project Management: Telling, Doodly, Storyline and Video-Sound", "TI Project Management Excellence",
+               "Strategic IT Project Leadership", "Agile Project Management for Technology Professionals", "Leaders As A Coach & Sales Team Developer",
+               "Leaders as a Coach & Team Developer in Collection",
+               "Conflict, Executive Coaching, Change Management and Business Transformation",
+               "Mediation Mastery: Resolving Disputes Effectively", "Negotiation Skills for Business Professionals", "Presentation & Selling Skills Using Story-Telling",
+               "Effective Communication & Interpersonal Skills", "Advanced Business Communication Strategies",
+               "Strategic Communication Excellence", "Professional Communication Mastery", "Understanding Millenials - Managing for High Performance and Engagement",
+               "Standard Operating Procedures for Efficient Operations", "Operational Excellence: From Strategy to Execution",
+               "Optimizing Business Processes for Efficiency", "Procedures and Processes Optimization Workshop", "Leaders As A Coach & Sales Team Developer",
+               "Leaders as a Coach & Team Developer in Collection", "Conflict, Executive Coaching, Change Management and Business Transformation", "Mediation Mastery: Resolving Disputes Effectively",
+               "Negotiation Skills for Business Professionals", "Presentation & Selling Skills Using Story-Telling",
+               "Effective Communication & Interpersonal Skills", "Advanced Business Communication Strategies",
+               "Strategic Communication Excellence", "Professional Communication Mastery", "Understanding Millenials - Managing for High Performance and Engagement",
+               "Standard Operating Procedures for Efficient Operations", "Operational Excellence: From Strategy to Execution",
+               "Optimizing Business Processes for Efficiency", "Procedures and Processes Optimization Workshop", "Conflict, Executive Coaching, Change Management and Business Transformation",
+               "Executive Coaching for Business Transformation", "Strategic Change Management Workshop", "Mediation Mastery: Effective Dispute Resolution",
+               "Negotiation Skills for Business Professionals", "Warehouse Administration for Better Complete Beginner",
+               "Optimisasi Operasional: Strategi Peningkatan Produktivitas", "Pelatihan Proses Produksi yang Lebih Baik",
+               "Mengelola Operasional Bisnis dengan Efektif", "Teknik Efisiensi Proses Produksi Terbaik", "How To Recruit, Interview, Select & Place the Right Employee",
+               "Effective Recruitment Strategies Workshop", "Advanced Employee Selection Techniques Training",
+               "Recruitment Excellence: Finding the Right Fit", "Mastering the Art of Employee Hiring and Selection", "Marketing Research For Business Using SPSS",
+               "Enterprise Solution Selling", "Advanced Marketing and Advertising Techniques", "Strategic Sales Promotion Training",
+               "Mastering the Art of Marketing Communication", "Powerful Database Analysis & Dashboard Reporting with Excel",
+               "Pelatihan Teknik Analisis yang Mendalam", "Keterampilan Analitis Proaktif",
+               "Strategi Analisis Data yang Efektif", "Pelatihan Keterampilan Analisis Bisnis", "System Application & Technology Knowledge & Skill for Transaction",
+               "IT Systems Mastery Program", "Advanced Infrastructure Understanding Workshop",
+               "Strategic IT Infrastructure Management Training", "IT Infrastructure Excellence Certification", "Information Security Awareness",
+               "Understanding Millenials - Managing for High Performance and Engagement", "Kesehatan dan Keselamatan Kerja: Panduan Praktis",
+               "Pelatihan Keselamatan Kerja di Lingkungan Kerja", "Kesehatan Pekerjaan dan Keselamatan: Aspek Penting", "Risk Analysis: to Reduce Losses and Increase Profits",
+               "Risk & Governance in Digital Era", "Pelatihan Strategi Mengurangi Kerugian dan Meningkatkan Keuntungan",
+               "Risiko dan Tata Kelola di Era Digital", "Pelatihan Analisis Risiko Proyek yang Efektif", "Project Management & Assertive User Handling",
+               "Modern Project Management: Telling, Doodly, Storyline and Video-Sound",
+               "TI Project Management Excellence", "Strategic IT Project Leadership",
+               "Agile Project Management for Technology Professionals", "Leaders As A Coach & Sales Team Developer",
+               "Leaders as a Coach & Team Developer in Collection",
+               "Conflict, Executive Coaching, Change Management and Business Transformation",  "Mediation Mastery: Resolving Disputes Effectively","Negotiation Skills for Business Professionals",
+               "Risk Analysis: to Reduce Losses and Increase Profits",
+               "Financial Management Mastery", "Strategic Risk and Financial Analysis",
+               "ProfitOptics: Financial Decision Excellence", "Quantitative Financial Modeling", "Modern Project Management: Telling, Doodly, Storyline and Video-Sound",
+               "Project Lifecycle Insights", "Effective Project Execution Strategies",
+               "ProActive Project Management", "Project Lifecycle Mastery", "Enterprise Solution Selling", 
+               "Supply Chain Management Mastery", "Strategic Supply Chain Optimization", 
+               "Supply Chain Excellence: From Sourcing to Delivery", "Advanced Supply Chain Strategies"
+
+ ]  # Ganti dengan pilihan yang sesuai
+    need = st.selectbox("Pilih kebutuhan pelatihan:", options)
+
+    # Button untuk mendapatkan rekomendasi
+    if st.button("Dapatkan Rekomendasi"):
+        data = pd.read_csv('dataset/AISALES_FITUR 6.csv')
+        cosine_sim = joblib.load('models/AISALES_FITURR6.joblib')
+        recommendations = need_recommendation(need, data, cosine_sim)
+
+        # Menampilkan header dengan warna dan style
+        st.markdown("<h2 style='color: #FFFFFF;'>Rekomendasi Pelatihan:</h2>", unsafe_allow_html=True)
+        st.markdown("---")
+        st.markdown(
+            """
+            <style>
+            table {
+                color: #FFFFFF;
+                background-color: #182c25;  /* Warna latar belakang tabel */
+            }
+            </style>
+            """,
+            unsafe_allow_html=True
+        )
+
+        # Menampilkan rekomendasi dalam bentuk tabel
+        st.table(pd.DataFrame(recommendations))
+
+        
+
+
+        
+
+        
+if __name__ == '__main__':
+    main()
